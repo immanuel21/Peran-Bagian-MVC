c0a8a108ffa94ee20431199da3393817e279838c
immanuel21 <immanuel.manafe@students.amikom.ac.id>
2020-12-16 14:55:03 +0700

    Add project files.

---
+++ Peran Bagian MVC/Controller/PenawaranController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Peran_Bagian_MVC.Model;
+
+namespace Peran_Bagian_MVC.Controller
+{
+    class PenawaranController
+    {
+        private List<Item> items;
+
+        public PenawaranController()
+        {
+            items = new List<Item>();
+        }
+
+        public void addItem(Item item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.items;
+        }
+    }
+}
